d21a1d36cd76e2cb1ad66c936c589fba21689453
Yogi Prakoso <yogiarrazy@gmail.com>
2023-12-13 14:48:56 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,28 +1,29 @@
 const $ = (x) => document.querySelector(x);
+const $$ = (x) => document.querySelectorAll(x);
 
 function addFilter(data) {
         const tag = data.getAttribute('tag');
-        const existingElement = document.querySelector(`#sort_tag [tag="${tag}"]`);
-        
+        const existingElement = $(`#sort_tag [tag="${tag}"]`);
+
         if (!existingElement) {
-            const filter = document.createElement('div');
-            filter.className = 'list_tag relative';
-            filter.setAttribute('tag', tag);
-            filter.innerText = data.textContent;
-    
-            const iconRemove = document.createElement('img');
-            iconRemove.src = './images/icon-remove.svg';
-            iconRemove.className = 'icon_remove';
-            iconRemove.alt = 'icon-remove';
-            iconRemove.setAttribute('onclick', 'removeAdd(this)');
-            filter.appendChild(iconRemove);
-    
-            $('#group_list').classList.add('space-1');
-            $('#sort_tag').appendChild(filter);
-            $('#group_filter').classList.remove('hidden');
+                const filter = document.createElement('div');
+                filter.className = 'list_tag relative';
+                filter.setAttribute('tag', tag);
+                filter.innerText = data.textContent;
+
+                const iconRemove = document.createElement('img');
+                iconRemove.src = './images/icon-remove.svg';
+                iconRemove.className = 'icon_remove';
+                iconRemove.alt = 'icon-remove';
+                iconRemove.setAttribute('onclick', 'removeAdd(this)');
+                filter.appendChild(iconRemove);
+
+                filterByTag(tag);
+                $('#group_list').classList.add('space-1');
+                $('#sort_tag').appendChild(filter);
+                $('#group_filter').classList.remove('hidden');
         }
-    }
-    
+}
 
 function removeFilter() {
         $('#group_filter').classList.add('hidden');
@@ -32,6 +33,27 @@ function removeFilter() {
 
 function removeAdd(data) {
         const d = data.parentElement;
+        const n = $$('.list_tag').length;
         $('#sort_tag').removeChild(d);
-
+        if (n === 1) {
+                removeFilter();
+        }
 }
+
+function filterByTag(tag) {
+        const jobElements = document.querySelectorAll('.list-job');
+        jobElements.forEach(function (jobElement) {
+                const tagElements = jobElement.querySelectorAll('.tag span');
+                let showJob = false;
+                tagElements.forEach(function (tagElement) {
+                        const elementTag = tagElement.getAttribute('tag');
+                        if (elementTag === tag) {
+                                showJob = true;
+                        }
+                });
+                if (!showJob) {
+                        jobElement.classList.add('hidden');
+                }
+        });
+}
+
