dbb3c8917b197bd212a4240a5b969f7e6a9d0454
Yogi Prakoso <yogiarrazy@gmail.com>
2023-12-13 14:30:44 +0700

    update

--- js/app.js
+++ js/app.js
@@ -2,22 +2,27 @@ const $ = (x) => document.querySelector(x);
 
 function addFilter(data) {
         const tag = data.getAttribute('tag');
-        const filter = document.createElement('div');
-        filter.className = 'list_tag relative';
-        filter.setAttribute('tag', `${tag}`);
-        filter.innerText = `${data.textContent}`;
-        const iconRemove = document.createElement('img');
-        iconRemove.src = './images/icon-remove.svg';
-        iconRemove.className = 'icon_remove';
-        iconRemove.alt = 'icon-remove';
-        iconRemove.setAttribute('onclick', 'removeAdd(this)');
-        filter.appendChild(iconRemove);
-
-        $('#group_list').classList.add('space-1');
-        $('#sort_tag').appendChild(filter);
-        $('#group_filter').classList.remove('hidden');
-        console.log($('#sort_tag'));
-}
+        const existingElement = document.querySelector(`#sort_tag [tag="${tag}"]`);
+        
+        if (!existingElement) {
+            const filter = document.createElement('div');
+            filter.className = 'list_tag relative';
+            filter.setAttribute('tag', tag);
+            filter.innerText = data.textContent;
+    
+            const iconRemove = document.createElement('img');
+            iconRemove.src = './images/icon-remove.svg';
+            iconRemove.className = 'icon_remove';
+            iconRemove.alt = 'icon-remove';
+            iconRemove.setAttribute('onclick', 'removeAdd(this)');
+            filter.appendChild(iconRemove);
+    
+            $('#group_list').classList.add('space-1');
+            $('#sort_tag').appendChild(filter);
+            $('#group_filter').classList.remove('hidden');
+        }
+    }
+    
 
 function removeFilter() {
         $('#group_filter').classList.add('hidden');
