ac293231382b9db1d6c2d61419bc85817f7bd12d
Yogi Prakoso <yogiarrazy@gmail.com>
2023-12-13 13:54:30 +0700

    update

--- js/app.js
+++ js/app.js
@@ -6,8 +6,18 @@ function addFilter(data) {
                 <div class="list_tag relative" tag="${tag}">${data.textContent}
                         <img src="./images/icon-remove.svg" class="icon_remove" alt="icon-remove">
                 </div>`;
+
+        const filter = document.createElement('div');
+        filter.className = 'list_tag relative';
+        filter.setAttribute('tag', `${tag}`);
+        filter.innerText = `${data.textContent}`;
+        const iconRemove = document.createElement('img');
+        iconRemove.src = './images/icon-remove.svg';
+        iconRemove.className = 'icon_remove';
+        iconRemove.alt = 'icon-remove';
+
+        $('#group_list').classList.add('space-1');
+        $('#sort_tag').appendChild(filter);
         $('#group_filter').classList.remove('hidden');
-        $('#group_list').classList.add('mt-20');
-        // $('#sort_tag').appendChild(elementFilter);
         console.log($('#sort_tag'));
 }
